9bbd0b5ad3892111170799696cba1a411b0743b4
Antonio Sejas <l040187@Moshnast.local>
2016-02-10 13:38:53 +0000

    03 success

--- 03-HANDLING/app.js
+++ 03-HANDLING/app.js
@@ -1,23 +1,28 @@
-    var Hapi = require('hapi');
-    var server = new Hapi.Server();
-    
-    server.connection({
-        host: 'localhost',
-        port: Number(process.argv[2] || 8080)
-    });
+var Hapi = require('hapi')
+var server = new Hapi.Server()
+
+var Inert = require('inert')
+var Path = require('path')
+
+server.register(Inert, function (err) {
+    if (err) throw err;
+});
+
+server.connection({
+    host: 'localhost',
+    port: Number(process.argv[2] || 8080)
+});
 
 
-    server.route({path: '/', method:'GET', handler: handlerGet});
-
-
-    function handlerGet(request, reply) {
-    
-        // Request has all information
-        // Reply handles client response
-    
-        reply('Hello hapi');
+server.route({
+    path: '/',
+    method:'GET',
+    handler: {
+        file: Path.join(__dirname,'views','index.html')
     }
+});
 
-    server.start(function () {
-        console.log('Server running at:', server.info.uri);
-    });
+server.start(function () {
+    console.log('Server running at:', server.info.uri);
+    console.log('Path.join : ',Path.join(__dirname,'views','index.html'))
+});
